44ddf4b5595652434b535c90c726344b29ab6e4a
Anjali01092001 <122157284+Anjali01092001@users.noreply.github.com>
2025-02-10 09:09:02 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_3kqrfx3Ay', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2v9q4ek4o4qs9fp6a5arcs3oaa', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-south-1' // e.g. us-east-2
+        userPoolId: 'ap-south-1_3kqrfx3Ay', 
+        userPoolClientId: '2v9q4ek4o4qs9fp6a5arcs3oaa',
+        region: 'ap-south-1',
+        clientSecret: 'm0tg6g0unq9epicp7n195493u5lcd5svllj2j4b4jv17sbegmcu'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' 
     }
 };
