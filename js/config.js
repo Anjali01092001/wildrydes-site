5d0a55807afe1798c1100f0706887d89cefa7df9
Anjali01092001 <122157284+Anjali01092001@users.noreply.github.com>
2025-02-10 10:04:36 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,9 +3,8 @@ window._config = {
         userPoolId: 'ap-south-1_RpI9q57Ea', 
         userPoolClientId: '5smuejdpv0qb2t1q61nt61acds',
         region: 'ap-south-1'
-        //clientSecret: 'm0tg6g0unq9epicp7n195493u5lcd5svllj2j4b4jv17sbegmcu'
     },
     api: {
-        invokeUrl: '' 
+        invokeUrl: 'https://on1pbqcjxe.execute-api.ap-south-1.amazonaws.com/dev' 
     }
 };
