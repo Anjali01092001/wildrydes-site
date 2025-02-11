9c717326ab550d3f4a1200dd8656538de62a435c
Anjali01092001 <122157284+Anjali01092001@users.noreply.github.com>
2025-02-11 08:29:19 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_RpI9q57Ea', 
-        userPoolClientId: '5smuejdpv0qb2t1q61nt61acds',
+        userPoolId: 'ap-south-1_cYRN801Od', 
+        userPoolClientId: '5ia2s0jci0m94l5pu684qcnsct',
         region: 'ap-south-1'
     },
     api: {
-        invokeUrl: 'https://on1pbqcjxe.execute-api.ap-south-1.amazonaws.com/dev' 
+        invokeUrl: '' 
     }
 };
